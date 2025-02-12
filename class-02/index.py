149d9dd23dc818cd4181dbd7ee9100d237b8c8cd
Alishba-Anjum <alishbaahmedkhan11@gmail.com>
2025-02-13 03:24:17 +0500

    class-01 , class-02 , class-03

---
+++ class-02/index.py
@@ -0,0 +1,28 @@
+
+# variables
+
+fav_celebrity = "Imran khan"
+fav_game = "Pubg"
+fav_food = "Biryani"
+
+print("Fav celebrity", fav_celebrity)
+print("fav game" , fav_game)
+print("fav food", fav_food)
+
+# Data types
+# String
+# Integer
+# Float
+# Boolean
+
+# String
+name = "Ali"
+
+# Integer
+age = 25
+
+# Float
+percentage = 75.5
+
+# Boolean
+is_student = True
---
+++ class-03/app.py
@@ -0,0 +1,27 @@
+#Boolean
+
+# num = 10
+# num2 = 10
+# print(num >= num2)
+
+#Conditions
+
+percentage = 29
+
+if(percentage >= 90 and percentage <= 100 ):
+    print("your grade is A+")
+
+elif(percentage >= 80 and percentage <= 90):
+    print("your grade is A") 
+
+elif(percentage >= 70 and percentage <= 80):
+    print("your grade is B")
+
+elif(percentage >= 60 and percentage <= 70):
+    print("your grade is C")
+
+elif(percentage >= 50 and percentage <= 60):
+    print("your grade is D")
+
+else:
+    print("Fail")
