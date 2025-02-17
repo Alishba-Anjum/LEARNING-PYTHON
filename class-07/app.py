4a198e5ccb5371fef3e0e66c8de4ad40a778d3a7
Alishba-Anjum <alishbaahmedkhan11@gmail.com>
2025-02-18 04:06:47 +0500

    class 07

--- class-07/app.py
+++ class-07/app.py
@@ -6,18 +6,25 @@
 # Example
 # Create a function named my_function:
 
-def my_function():
+def greet():
   print("Hello from a function")
 
 # Call the function:
-my_function()
+greet()
 
 # functions can take parameters
 
-def my_function(name):
+def greet(name):
   print(f'Hello {name}')
 
-my_function('Alishba')  
+greet('Alishba')  
+
+# default parameters
+def greet(name = 'Guest'):
+  print(f'Hello {name}')
+
+greet() # Hello Guest
+greet('Alishba')  # Hello Alishba
 
 # functions can return values
 
@@ -26,3 +33,38 @@ def add(a , b):
 
 result = add(2, 3)
 print(result)
+
+def make_tea():
+  print('Boil water')
+  print('Add tea leaves')
+  print('Add sugar')
+  print('Add milk')
+  print('Boil for 5 minutes')
+  print('Serve')
+  return 'Tea is ready ☕ '
+
+tea = make_tea()
+print(tea)  # Tea is ready ☕
+
+
+
+# mini project
+def call_llm(prompt):
+  print(f'User : {prompt}') 
+  return 'AI : Hello, How can I help you?'
+
+reposne = call_llm(prompt= 'Hi, My name is Alishba.')  
+print(reposne)
+
+
+
+
+# dice roll game
+import random
+
+def roll_dice(limit = 6):
+  return random.randint(1, limit)
+
+
+print(roll_dice(40))  
+print(roll_dice())
